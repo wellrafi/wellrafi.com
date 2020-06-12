4ed36aa0c04bfb93daec3cf0ee4c2a982a7d44af
wellrafi <wellrafiuniverse@gmail.com>
2020-06-12 04:05:00 +0000

    add scss

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/bootstrap.scss'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
--- pages/aboutme.js
+++ pages/aboutme.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Prev App</title>
+        <title>Fullstack Developer - Rafi Royhan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
